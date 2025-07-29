d72c2781e89602fe132a1a7c5ac5568df24e1ab4
Andax service Account <IT@andaxtech.com>
2025-07-28 20:02:58 -0700

    Increase image upload size

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 
 // Route mounting
 app.use('/api', authRoutes);
