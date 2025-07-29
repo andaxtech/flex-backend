565a3315f3906c1290bf3159cf9ae5d30b038ebf
Andax service Account <IT@andaxtech.com>
2025-07-28 17:52:06 -0700

    implemented face recognition for check in- update cloudinary

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -3,9 +3,9 @@ const cloudinary = require('cloudinary').v2;
 const multer = require('multer');
 
 cloudinary.config({
-  cloud_name: process.env.dyqawnoxj,
-  api_key: process.env.312896732184767,
-  api_secret: process.env.PHanRLll2Xu-x-KwWNft-Pg4Fks
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 // Use memory storage instead of cloudinary storage
